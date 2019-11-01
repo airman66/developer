bec3f385f168fc2023d234618065cb56f7d8c178
airman66 <53268089+airman66@users.noreply.github.com>
2019-11-01 14:24:52 +0300

    Сделали чтобы видимый пользователем элемент был активен в меню

--- js/index.js
+++ js/index.js
@@ -1,6 +1,36 @@
 $(function () {
+	if ($(window).scrollTop() < $("#intro").innerHeight()) {
+		$("a.nav__link[href='#intro']").addClass("nav__link--active");
+	} else $("a.nav__link[href='#intro']").removeClass("nav__link--active");
+	if ($(window).scrollTop() > $("#intro").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight()) {
+		$("a.nav__link[href='#about']").addClass("nav__link--active");
+	} else $("a.nav__link[href='#about']").removeClass("nav__link--active");
+	if ($(window).scrollTop() > $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight()) {
+		$("a.nav__link[href='#working']").addClass("nav__link--active");
+	} else $("a.nav__link[href='#working']").removeClass("nav__link--active");
+	if ($(window).scrollTop() > $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() + $("#gallery").innerHeight()) {
+		$("a.nav__link[href='#gallery']").addClass("nav__link--active");
+	} else $("a.nav__link[href='#gallery']").removeClass("nav__link--active");
+	if ($(window).scrollTop() > $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() + $("#gallery").innerHeight() + $(".companies").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() + $("#gallery").innerHeight() + $(".companies").innerHeight() + $("#contact").innerHeight()) {
+		$("a.nav__link[href='#contact']").addClass("nav__link--active");
+	} else $("a.nav__link[href='#contact']").removeClass("nav__link--active");
 	$(window).scroll(function () {
-		if ($(window).scrollTop() > $(".intro").innerHeight()) {
+		if ($(window).scrollTop() > $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() + $("#gallery").innerHeight() + $(".companies").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() + $("#gallery").innerHeight() + $(".companies").innerHeight() + $("#contact").innerHeight()) {
+			$("a.nav__link[href='#contact']").addClass("nav__link--active");
+		} else $("a.nav__link[href='#contact']").removeClass("nav__link--active");
+		if ($(window).scrollTop() > $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight() + $("#gallery").innerHeight()) {
+			$("a.nav__link[href='#gallery']").addClass("nav__link--active");
+		} else $("a.nav__link[href='#gallery']").removeClass("nav__link--active");
+		if ($(window).scrollTop() > $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight() + $(".results").innerHeight() + $(".skills").innerHeight() + $("#working").innerHeight()) {
+			$("a.nav__link[href='#working']").addClass("nav__link--active");
+		} else $("a.nav__link[href='#working']").removeClass("nav__link--active");
+		if ($(window).scrollTop() < $("#intro").innerHeight()) {
+			$("a.nav__link[href='#intro']").addClass("nav__link--active");
+		} else $("a.nav__link[href='#intro']").removeClass("nav__link--active");
+		if ($(window).scrollTop() > $("#intro").innerHeight() - 72 && $(window).scrollTop() < $("#intro").innerHeight() + $("#about").innerHeight()) {
+			$("a.nav__link[href='#about']").addClass("nav__link--active");
+		} else $("a.nav__link[href='#about']").removeClass("nav__link--active");
+		if ($(window).scrollTop() > $(".intro").innerHeight() - 72) {
 			$(".header").addClass("header--fixed");
 		} else {
 			$(".header").removeClass("header--fixed");
@@ -14,12 +44,21 @@ $(function () {
 		$(".burger-menu").css("animation", "burger-menu__animation-reverse linear .5s");
 		$(".burger-menu").css("left", "-100%");
 	});
-	$(".nav>a, .burger-menu>li>a").click(function (e) { 
+	$(".nav>a, .burger-menu>li>a").click(function (e) {
 		e.preventDefault();
-		$("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 1000);
+		if ($(this).attr("href") == "#about") {
+			$("html, body").animate({
+				scrollTop: $($(this).attr("href")).offset().top
+			}, 1000);
+		}
+		else {
+			$("html, body").animate({
+				scrollTop: $($(this).attr("href")).offset().top
+			}, 1000);
+		}
 	});
 	$(".burger-menu>li>a").click(function () {
 		$(".burger-menu").css("animation", "burger-menu__animation-reverse linear 0.5s");
 		$(".burger-menu").css("left", "-100%");
-	});
+	})
 });
