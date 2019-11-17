65bb69a6420f87ef0d21b06e41c459e3487b25bf
airman66 <53268089+airman66@users.noreply.github.com>
2019-11-17 15:31:24 +0300

    Пофиксили баг с кнопкой НАВЕРХ

--- app/js/toTopBtn.js
+++ app/js/toTopBtn.js
@@ -2,19 +2,18 @@ let createToTopBtn = speed => {
     $("body").append("<div class='toTopBtn'></div>");
     $(".toTopBtn").on("click", function () {
         if (speed != 1000) {
-            $("html, body").animate({scrollTop: 0}, speed);
-        } else $("html, body").animate({scrollTop: 0}, 1000);
+            $("html, body").animate({
+                scrollTop: 0
+            }, speed);
+        } else $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $(".intro").innerHeight() && $(window).scrollTop() < $("body").innerHeight() - 1 - $(window).innerHeight()) {
+            $(".toTopBtn").css("display", "block");
+        } else {
+            $(".toTopBtn").css("display", "none");
+        }
     });
 };
-
-$(window).scroll(function () {
-    if ($(window).scrollTop() > $(".intro").innerHeight()) {
-        $(".toTopBtn").css("display", "block");
-    }
-    else {
-        $(".toTopBtn").css("display", "none");
-    }
-    if ($(window).scrollTop() > $("body").innerHeight() - $("footer").innerHeight() - $(window).innerHeight()) {
-        $(".toTopBtn").css("display", "none");
-    } else $(".toTopBtn").css("display", "block");
-});
